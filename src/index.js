337ca94765ba80407b2c943f94993e4be1c16434
shivang datta <shivangdatta@gmail.com>
2024-07-06 02:49:37 +0530

    production ready v0

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import { BrowserRouter , Routes , Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
+import {disableReactDevTools} from '@fvilers/disable-react-devtools'
+
+if(process.env.NODE_ENV === 'production') disableReactDevTools
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
