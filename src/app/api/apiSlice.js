2002644900b5febfefb0c3e8cf40970e833d172b
shivang datta <shivangdatta@gmail.com>
2024-07-06 03:13:14 +0530

    hosting server changed

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-    baseUrl : 'https://technotes-api-v0.onrender.com',
+    baseUrl : 'https://technotes-api-v0-nt2n.onrender.com/',
     credentials : "include",
     prepareHeaders : (headers , {getState}) => {
         const token = getState().auth.token
